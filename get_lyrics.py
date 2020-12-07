fa08a3654ec135634d39de8cc464a331d7d9bb70
mfriebel <matfriebel@gmail.com>
2020-12-07 18:44:13 +0100

    python for extracting lyrics html for artist on metrolyrics.com

---
+++ get_lyrics.py
@@ -0,0 +1,27 @@
+import requests, time, re, os
+# Identifier 
+header = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:83.0) Gecko/20100101 Firefox/83.0"}
+
+artist = 'beatles'
+
+# Get HTML of lyrics page
+lyrics_url = f"https://www.metrolyrics.com/{artist}-lyrics.html"
+http_request = requests.get(lyrics_url, headers=header)
+
+# find each lyrics URL inside the artist page using RE and save to a list
+pattern = f'<a href=\"(.+-lyrics-{artist}\.html)\"'
+lyrics_links = re.findall(pattern, http_request.text)
+
+# Create new folder for a new artist
+os.mkdir(f'week04/artists/{artist}/')
+
+
+# Loop through lyrics URL list and write for each song a new html file 
+for url in lyrics_links:
+    time.sleep(1)
+    song_title = re.findall(f'com/(.+)-lyrics-{artist}', url)[0]        # Get song title
+    song_html= requests.get(url, headers=header)                         # Get song lyrics html
+    #print(re.findall('<p class=\'verse\'>(.*)<br>|^(.*)</p>|^(.*)<br>', song_html.text))                             
+    with open(f'week04/artists/{artist}/{song_title}.html', 'w') as f:  # Write lyrics page to ".html" file
+        f.write(song_html.text)
+        f.close()
