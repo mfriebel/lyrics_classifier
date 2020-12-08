c98e150faa549607d1f0305a75cc4c5efef2097b
mfriebel <matfriebel@gmail.com>
2020-12-08 21:02:41 +0100

    created functions for downloading the lyrics

--- get_lyrics.py
+++ get_lyrics.py
@@ -2,26 +2,35 @@ import requests, time, re, os
 # Identifier 
 header = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:83.0) Gecko/20100101 Firefox/83.0"}
 
-artist = 'beatles'
+def get_lyrics_links(artist):
+    # Get HTML of lyrics page
+    lyrics_url = f"https://www.metrolyrics.com/{artist}-lyrics.html"
+    http_request = requests.get(lyrics_url, headers=header)
 
-# Get HTML of lyrics page
-lyrics_url = f"https://www.metrolyrics.com/{artist}-lyrics.html"
-http_request = requests.get(lyrics_url, headers=header)
+    # find each lyrics URL inside the artist page using RE and save to a list
+    pattern = f'<a href=\"(.+-lyrics-{artist}\.html)\"'
+    lyrics_links = re.findall(pattern, http_request.text)
 
-# find each lyrics URL inside the artist page using RE and save to a list
-pattern = f'<a href=\"(.+-lyrics-{artist}\.html)\"'
-lyrics_links = re.findall(pattern, http_request.text)
-
-# Create new folder for a new artist
-os.mkdir(f'week04/artists/{artist}/')
+    return lyrics_links
 
 
-# Loop through lyrics URL list and write for each song a new html file 
-for url in lyrics_links:
-    time.sleep(1)
-    song_title = re.findall(f'com/(.+)-lyrics-{artist}', url)[0]        # Get song title
-    song_html= requests.get(url, headers=header)                         # Get song lyrics html
-    #print(re.findall('<p class=\'verse\'>(.*)<br>|^(.*)</p>|^(.*)<br>', song_html.text))                             
-    with open(f'week04/artists/{artist}/{song_title}.html', 'w') as f:  # Write lyrics page to ".html" file
-        f.write(song_html.text)
-        f.close()
+def download_lyrics_html(artist):
+    # Create new folder for a new artist    
+    os.mkdir(f'week04/artists/{artist}/')
+    # retrive lyrics links of given artist
+    lyric_links = get_lyrics_links(artist)
+    # Loop through lyrics URL list and write for each song a new html file 
+    for url in lyric_links:
+        time.sleep(1)
+        song_title = re.findall(f'com/(.+)-lyrics-{artist}', url)[0]        # Get song title
+        song_html= requests.get(url, headers=header)                         # Get song lyrics html
+        #print(re.findall('<p class=\'verse\'>(.*)<br>|^(.*)</p>|^(.*)<br>', song_html.text))                             
+        with open(f'week04/artists/{artist}/{song_title}.html', 'w') as f:  # Write lyrics page to ".html" file
+            f.write(song_html.text)
+            f.close()
+
+artists = ['beatles', 'queen', 'xtc', 'solange', 'harry-nilsson', 'baxter-dury', 'david-bowie', 'jonathan-richman', 
+            'gang-of-four', 'clash', '10-cc', 'scott-walker', 'fleetwood-mac']
+
+for artist in artists:
+    download_lyrics_html(artist)
