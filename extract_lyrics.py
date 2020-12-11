2d54251e06d109edaf78e2eb2d9e45466b815d9c
mfriebel <matfriebel@gmail.com>
2020-12-11 22:09:28 +0100

    added function for getting all lyrics and labels as lists and a function for preprocessing the lyrics list

--- extract_lyrics.py
+++ extract_lyrics.py
@@ -1,8 +1,13 @@
+"""
+Extracts lyrics from html and preprocesses text
+"""
 import os
 from bs4 import BeautifulSoup
 import spacy
 import re
 en = spacy.load('en_core_web_sm')
+
+PATH = './artists/'
 
 def extract_lyric_text(html):
     # Parse HTML file
@@ -18,13 +23,14 @@ def extract_lyric_text(html):
     return lyric_str
 
 def get_all_lyric_texts(artist):
-    # Path to lyrics of each downloaded artist
-    path = f'artists/{artist}/'
     # Empty list to collect all lyrics of the artist
     lyrics_all = []
+    artists_list = []
     # Loop through artist folder
-    for fn in os.listdir(path):
-        text = open(path + fn).read()
+    song_path = os.path.join(PATH, artist)
+    print(f'\nreading songs by {artist}')
+    for fn in os.listdir(song_path):
+        text = open(song_path+ '/' + fn).read()
         # Extract lyrics text
         lyric_str = extract_lyric_text(text)
         # remove newline command
@@ -32,19 +38,23 @@ def get_all_lyric_texts(artist):
         # Append to list 
         if len(lyric_str) > 0: #Skip empty lyric pages
             lyrics_all.append(lyric_str)
+            artists_list.append(artist)
+    print(f'read {len(lyrics_all)} songs')
+    return artists_list, lyrics_all
 
-    return lyrics_all
-
-
-def create_artist_lyrics_dict(artists):
-    artist_lyrics_dict = {}
+def get_labels_lyrics_lists(artists):
+    labels_artists = []
+    corpus_artists = []
 
     for artist in artists:
-        artist_lyrics_dict[artist] = get_all_lyric_texts(artist)
+        label_list, corpus_list = get_all_lyric_texts(artist)
+        labels_artists.append(label_list)
+        corpus_artists.append(corpus_list)
 
-    return artist_lyrics_dict
+    labels = sum(labels_artists, [])
+    corpus = sum(corpus_artists, [])
 
-
+    return labels, corpus
 
 def lyrics_preproc(lyric_string):
     # Preprocessing with spacy
@@ -52,19 +62,34 @@ def lyrics_preproc(lyric_string):
     # Set to lower case letters
     lyric_string = lyric_string.lower()
     # Remove numbers
-    lyric_string = re.sub('[0-9]+', '', lyric_string)
+    lyric_string = re.sub('[0-9]+', '', lyric_string) 
 
     # Convert to spacy text object
     doc = en(lyric_string)
     # Create a new list for string without stopwords and only the word lemmatisation
     new_text = []
-    for token in doc:
+    for token in doc:                       #TODO: take apart
         if (token.is_stop == False):
             new_text.append(token.lemma_)
     # join list of strings into a string for the lyric    
     return ' '.join(new_text)
 
+def lyrics_all_preprocess(lyric_list):
+    preprocess_list = []
+    for i, value in enumerate(lyric_list):
+            preprocess_list.append(lyrics_preproc(value))
+    
+    return preprocess_list
+
+
 ### Bag of Words
+def create_artist_lyrics_dict(artists, path):
+    artist_lyrics_dict = {}
+
+    for artist in artists:
+        artist_lyrics_dict[artist] = get_all_lyric_texts(artist, path)[1]
+
+    return artist_lyrics_dict
 
 def dict_preprocess(dict_lyrics):
     # Create empty list for list of artist names and list of lyrics of artist
@@ -86,3 +111,6 @@ def dict_preprocess(dict_lyrics):
     corpus = sum(values_list, [])
 
     return labels, corpus
+
+
+
